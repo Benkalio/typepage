247953b19b53b35682886432fcd8c333d66f85f2
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-11 23:50:12 +0100

    text added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello</div>
+  return <div>
+    <textarea></textarea>
+    <div>
+      <button>Submit</button>
+    </div>
+    <pre>New ook</pre>
+  </div>
 }
 
 ReactDOM.render(<App />, document.querySelector('root'))
