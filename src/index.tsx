12fc3aa1ab00de870087ac70f4d3c4b4aba82c41
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-12 01:25:03 +0100

    madded hooks to manage piece of states

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,46 @@
-import ReactDOM from "react-dom";
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom";
+import { render } from 'react-dom';
+
+
 
 const App = () => {
-  return <div>
-    <textarea></textarea>
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  
+  // this state is the output of the transpiled code from ESbuild 
+  // displayed in the pre>
+  const [code, setCode] = useState('');
+  
+  // esbuild wasm setup
+  // get esbuild from public file
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  }
+  useEffect(() => {
+    startService();
+  }, []);
+
+  // show code anytime a user clicks button 
+  const onClick = () => {
+    if (!ref.current) {
+      return console.log(ref.current);
+    }
+  }
+
+  return (
     <div>
-      <button>Submit</button>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
-    <pre>New ook</pre>
-  </div>
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector('root'))
+render(<App />, document.querySelector('#root'))
