dffd3a7f339bef92cacdee5baac3c2886ea250d2
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-24 15:11:19 +0100

    sandboxing an iframe

--- src/index.tsx
+++ src/index.tsx
@@ -64,7 +64,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe title="title" src="/test.html" />
+      <iframe title="title" sandbox='allow-same-origin' src="/test.html" />
     </div>
   );
 }
