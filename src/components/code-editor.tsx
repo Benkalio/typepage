b3dbd500e6de2bc98d80f16eb11f15a4774a1b59
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-26 00:07:10 +0100

    types files fixed and tabs to two space

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,38 +1,43 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount} from '@monaco-editor/react';
 
 interface CodeEditorProps {
   initialValue: string;
   onChange(value: string): void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
   /* Called once the Code Editor is first displayed 
     on the screen. First args is the initial value 
     and second args is the reference to the monaco editor displayed 
   */
-  const onEditorDidMount = (getValue: () => string, monacoEditor: any) => {
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
+
+    // spaces for tabs on code editor
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
-  return <MonacoEditor
-    editorDidMount={onEditorDidMount}
-    value={initialValue} //initial value of the interface
-    theme='dark'
-    language='javascript'
-    height='400px'
-    options={{
-      wordWrap: 'on',
-      minimap: { enabled: false },
-      showUnused: false,
-      folding: false,
-      lineNumbersMinChars: 3,
-      fontSize: 16,
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-    }}
-  />;
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue} //initial value of the interface
+      theme='dark'
+      language='javascript'
+      height='400px'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );  
 };
 
 export default CodeEditor;
