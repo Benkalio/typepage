e6610f504eec524b1ad61168a66fd645af21c85c
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-26 22:47:58 +0100

    extracting bundling logic to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,19 @@
-import * as esbuild from 'esbuild-wasm';
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState, useEffect, useRef } from "react";
 import { render } from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import { useState } from "react";
+import bundle from './bundler';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
 
 const App = () => {
-  const ref = useRef<any>();
   const [input, setInput] = useState('');
-  
-  // this state is the output of the transpiled code from ESbuild 
-  // displayed in <pre> tag
   const [code, setCode] = useState('');
 
-  
-  // esbuild wasm setup
-  // get esbuild from public file
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    });
-  }
-  useEffect(() => {
-    startService();
-  }, []);
-
-  // show code anytime a user clicks button 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
     // bundling process: this is to transpile input 
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(), fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE.ENV': '"production"',
-        global: 'window',
-      },
-    });
-    
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input)
+    setCode(output);
   };
 
   return (
