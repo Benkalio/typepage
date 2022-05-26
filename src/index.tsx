7a6fd771fea41f65de576506f7eb5761e96e5149
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-26 23:02:32 +0100

    multiple editors and preview window

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,14 @@
 import { render } from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from "react";
-import bundle from './bundler';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    // bundling process: this is to transpile input 
-    const output = await bundle(input)
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const x = 26;"
-        onChange={(value) => setInput(value)}  
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 };
 
-render(<App />, document.querySelector('#root'))
+render(<App />, document.querySelector('#root'));
