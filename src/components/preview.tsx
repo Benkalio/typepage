71e74c58ffc59f49e4493aa4956a21100aa93727
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-05-29 03:04:59 +0100

    a waalkaround fix to the background-color overflow of issue in the preview window

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -9,7 +9,9 @@ interface PreviewProps {
 // html structure written for events 
 const html = `
   <html>
-    <head></head>
+    <head>
+      <style>html { background-color: white; }</style>
+    </head>
     <body>
       <div id="root"></div>
       <script>
