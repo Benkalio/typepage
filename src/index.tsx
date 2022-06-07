88a23350b62df5bcc3b035172d36de0ddf61e51e
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-06-08 00:22:09 +0100

    React to Redux connection

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import { render } from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import { Provider } from 'react-redux';
+import { store } from './state';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
+    <Provider store={store}>
       <TextEditor />
       {/* <CodeCell /> */}
-    </div>
+    </Provider>
   );
 };
 
