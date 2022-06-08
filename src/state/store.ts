95e1cc9a86ede99f7c34a567411409c0837b2ac7
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-06-08 23:26:16 +0100

    Creating the action creators helpers

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,26 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-// import { ActionType } from './action-types';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 // manually testing dispatch store
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'code',
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
 
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'text',
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
 
 // // get the id of the first, second or third cell created
 // const id = store.getState().cells.order[0];
