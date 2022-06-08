df4e65db65774ed5dcb1e80118386f06e971629a
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-06-09 00:22:21 +0100

    applying custom styles to the buttons

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -3,6 +3,7 @@ import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
 import ActionBar from './action-bar';
+import './styles/cell-list-item.css';
 
 interface CellListItemProps {
   cell: Cell;
@@ -18,9 +19,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   }
 
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className="cell-list-item">
       {child}
+      <ActionBar id={cell.id} />
     </div>
   );
 };
