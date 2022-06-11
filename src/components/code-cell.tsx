bb29fe718c3d30a47c5a5fcad253eec32b44140c
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-06-11 23:37:55 +0100

    Adding cumulative code execution feature on preview screen

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,6 +3,7 @@ import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import './styles/code-cell.css';
 
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 import { Cell } from '../state';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -15,23 +16,24 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   // bundling logic: this is to transpile input
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
